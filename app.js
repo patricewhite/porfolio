638640bf5a4efc1a5c6520832994a38f27216fc1
patricewhite <patricewhite2012@gmail.com>
2017-07-07 11:25:22 -0400

    pagination working

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
 'use strict';
-$(function () {
-  $('.next-page').click(() => {
-    $('.about').toggle('current');
-    $('.Front-page').toggleClass('current');
-  });
+// $(function () {
+//   $('.next-page').click(() => {
+//     $('.about').toggle('current');
+//     $('.Front-page').toggleClass('current');
+//   });
+// });
+let app;
+$(function() {
+  app = {
+    currentPage: 1,
+    init: function (){
+      app.goNext();
+      app.goPrevious();
+    },
+    goNext: function() {
+    	$(document).on('click', '#next', function() {
+        app.currentPage = app.currentPage + 1;
+        return app.showPage(app.currentPage - 1, app.currentPage);
+      });
+    },
+    goPrevious: function() {
+     $(document).on('click', '#previous', function() {
+       app.currentPage = app.currentPage - 1;
+       return app.showPage(app.currentPage + 1, app.currentPage);
+     });
+   },
+   showPage: function(prevP, newP) { console.log(prevP, newP);
+    	let prevPage = document.getElementById(`page${prevP}`);
+      let newPage = document.getElementById(`page${newP}`);
+    	$(prevPage).addClass('inactive');
+      $(newPage).removeClass('inactive');
+    }
+  };
+});
+
+$(document).ready(function() {
+  app.init();
 });
